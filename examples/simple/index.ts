49e07d1aa02a487e878e0ba17e9c6ba2a281fadd
Tyler Kontra <tyler@zeet.co>
2023-02-03 11:20:16 -0800

    remove test from example

--- examples/simple/index.ts
+++ examples/simple/index.ts
@@ -30,11 +30,6 @@ const newProjectPreview = new zeet.resources.Project("new-project-for-preview", 
     name: "pulumi-test-preview"
 })
 
-const newProjectPreview2 = new zeet.resources.Project("new-project-for-preview2", {
-    userId: teamIdConfig,
-    name: "pulumi-test-preview234"
-})
-
 const environment = new zeet.resources.Environment("my-environment", {
     projectId: project.projectId,
     name: "pulumi-test-environment-02-renamed",
